12bde1ad15634945a27a9fcccffd0a06a37f54df
Riya72-coder <Riyakakde72@gmail.com>
2026-01-15 15:07:22 +0530

    Build SafeWalk core Logic(Voice + Gemini)

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,135 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:google_generative_ai/google_generative_ai.dart";
+import "package:speech_to_text/speech_to_text.dart" as stt;
+import "package:permission_handler/permission_handler.dart";
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const SafeWalkApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class SafeWalkApp extends SatetlessWidget {
+  coonst SafeWalkApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
+      home: const MapScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MapScreenState extends State<MapScreen> {
+  // logic variables
+  bool _isListening = false;
+  String _wordSpoken = "Tap the mic and say something...";
+  bool _isDanger = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions();
+  }
+
+  //1. Ask for permission
+  Future<void> _requestPermissions() async {
+    await[Permission.microphone, Permission.location].request();
+  }
+
+  //2. The voice logic
+  void _listen() async {
+    if(!_isListening) {
+      bool available = await _speech.initialize();
+      if(available) {
+         setState(() => _isListening = true);
+         _speech.listen(onResult: (val) {
+          setState(() {
+            _wordsSpoken = val.recognizedWords;
+            if(val.finalResult) {
+              _isListening = false;
+              _scheckSafetyWithGemini(_wordsSpoken);
+            }
+          });
+         });
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  } 
+
+  // 3. The AI brain
+  Future<void> _checkSafetyWithGemini(String text) async {
+    final model = GenerativeModel(
+      model: "gemini-1.5-flash",
+      apiKey: "AIzaSyAjoGKX9lZLWZz8Qk0WczPK4ajXeZzEYUY",
+    );
+
+    final prompt = "User Said: "$text". Analyze for danger(Help, Bacho, etc). Reply ONLY "Alert" or "OK". ";
+    final content = [Content.text(prompt)];
+    final response = await model.generateContent(content);
+
+    if(response.text?.contains("ALERT") ?? false) {
+      setState(() => _isDanger = true);
+    } else{
+      setState(() => _isDanger = false);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: _isDanger ? Colors.red[900]: Colors.grey[100],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("SafeWalk Guardian"),
+        backgroundColor: _isDanger ? Colors.red : Colors.indigo,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                _isDanger ? "DANGER DETECTED!" : _wordsSpoken,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: _isDanger ? Colors.white : Colors.black87,
+                  fontWeight: FontWeight.bold,                
+                ),
+              ),
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        Container(
+          padding: const EdgeInsets.all(30),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          ),
+          child: Column(
+            children: [
+              Text(_isDanger ? "Emergency Services Alerted" : "Guardian is Listening"),
+              const SizedBox(height: 20),
+              FloatingActionButton.extended(
+                onPressed: _listen,
+                label: Text(_isListening ? "Listening..." : "Talk to Guardian"),
+                icon: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                backgroundColos: _isListening ? Colors.red : Colors.indigo,
+              ),
+            ],
+          ),
+        ),
+        ],
       ),
     );
   }
 }
+
+
